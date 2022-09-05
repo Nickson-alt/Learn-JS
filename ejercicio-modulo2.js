0f6f2fe34846781e1c294b7382d07c8ece3f32e4
Nickson-alt <nicksonpachon1@gmail.com>
2022-09-05 13:57:18 -0500

    Ejercicio Modulo 2
    Ejercicio resuleto

---
+++ ejercicio-modulo2.js
@@ -0,0 +1,15 @@
+/*Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con
+propiedades: titulo, autor, fecha, url)*/
+
+let usuario,
+    nombre = "Nickson",
+    edad = 19;
+    "¿Eres desarrollador?" = True,
+    fecha_nacimiento = new Date(2003, 05, 14),
+    Libro_favorito = "El ego es el enemigo";
+
